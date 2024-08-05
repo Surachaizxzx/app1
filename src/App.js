5532f081687b0b5c203f9f1ed6828a73773f4ac2
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-08-05 10:19:31 +0700

    e
    e

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react"
+import './style.css'
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
+} 
