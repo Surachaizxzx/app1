99a45ac590fd31a19c3e90de4232a18b69444aa4
surachai subnalsing <143887257+Surachaizxzx@users.noreply.github.com>
2024-08-08 01:34:12 +0700

    ส่งงานคร้าบบจารย์

--- src/class-components.js
+++ src/class-components.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-export default class Calendar extends Component{
-    getDate(){
-        const dayNames=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',]
-        const monthNames= ["January","February","March","April","May","June","July",
-        "August","September","October","November","December"];
-        const WeekDay=dayNames[data.getDay()]
-        const day=date.getDate()
-        const month=monthNames[date.getMonth]
-        const year=date.getFullYear()+543
-        return  `วัน${WeekDay}ที่${day}${month}${year}`
-
-    }
-    render(){
-        return <div>{this.getDate()}</div>
-    }
-    
+export default class Calendar extends Component {
+  getDate() {
+    const dayNames = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const date = new Date();
+    const weekDay = dayNames[date.getDay()];
+    const day = date.getDate();
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+    return `${weekDay + `\t`}${day + `\t`}${month + `\t`}${year}`;
+  }
+  render() {
+    return <div>{this.getDate()}</div>;
+  }
 }
